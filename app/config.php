02788e351cdd66d02844a109a5cb9417ac21fe62
suoza1104-eng <suoza1104@gmail.com>
2026-06-01 23:12:00 -0300

    Add cPanel deploy config for V3

--- app/config.php
+++ app/config.php
@@ -53,7 +53,7 @@ if ($isLocal) {
 
 // Senha padrão do certificado (pode trocar depois)
 define('SENHA_CERTIFICADO', 'FERA2025');
-define('APP_VERSION', 'V2');
+define('APP_VERSION', 'V3');
 
 /**
  * Credenciais do ADMIN (login da área administrativa)
