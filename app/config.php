004964a38cd569a57f02fdaa07d7ac6b70b87416
suoza1104-eng <suoza1104@gmail.com>
2026-06-01 23:20:11 -0300

    Fix cPanel deploy permissions for V4

--- app/config.php
+++ app/config.php
@@ -53,7 +53,7 @@ if ($isLocal) {
 
 // Senha padrão do certificado (pode trocar depois)
 define('SENHA_CERTIFICADO', 'FERA2025');
-define('APP_VERSION', 'V3');
+define('APP_VERSION', 'V4');
 
 /**
  * Credenciais do ADMIN (login da área administrativa)
