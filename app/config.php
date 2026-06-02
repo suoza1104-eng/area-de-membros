7108e5892a0e8bf226e1de6e3008ca28fb87d4ac
suoza1104-eng <suoza1104@gmail.com>
2026-06-01 23:22:06 -0300

    Harden cPanel deploy chmod for V5

--- app/config.php
+++ app/config.php
@@ -53,7 +53,7 @@ if ($isLocal) {
 
 // Senha padrão do certificado (pode trocar depois)
 define('SENHA_CERTIFICADO', 'FERA2025');
-define('APP_VERSION', 'V4');
+define('APP_VERSION', 'V5');
 
 /**
  * Credenciais do ADMIN (login da área administrativa)
