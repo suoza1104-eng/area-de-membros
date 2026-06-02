03f0ed019e34c9977204b2f2854777c102369bbf
suoza1104-eng <suoza1104@gmail.com>
2026-06-01 23:25:03 -0300

    Simplify cPanel chmod for V6

--- app/config.php
+++ app/config.php
@@ -53,7 +53,7 @@ if ($isLocal) {
 
 // Senha padrão do certificado (pode trocar depois)
 define('SENHA_CERTIFICADO', 'FERA2025');
-define('APP_VERSION', 'V5');
+define('APP_VERSION', 'V6');
 
 /**
  * Credenciais do ADMIN (login da área administrativa)
