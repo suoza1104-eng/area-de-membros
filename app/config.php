5b6a182cb48b8a6255154b119db1dd3521357f32
suoza1104-eng <suoza1104@gmail.com>
2026-06-07 10:45:32 -0300

    Atualiza versao para v14

--- app/config.php
+++ app/config.php
@@ -53,7 +53,7 @@ if ($isLocal) {
 
 // Senha padrão do certificado (pode trocar depois)
 define('SENHA_CERTIFICADO', 'FERA2025');
-define('APP_VERSION', 'v13');
+define('APP_VERSION', 'v14');
 
 /**
  * Credenciais do ADMIN (login da área administrativa)
